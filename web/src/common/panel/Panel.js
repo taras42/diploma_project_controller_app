86127312b5f7e83185855bd217e373b551f98d1d
taras42 <taras.my.mail@gmail.com>
2015-05-26 13:18:51 +0300

    More panel impl.

--- web/src/common/panel/Panel.js
+++ web/src/common/panel/Panel.js
@@ -2,13 +2,17 @@ define(function(require) {
 
 	var Backbone = require("backbone"),
 		_ = require("underscore"),
+		ItemCollection = require("common/itemCollection/ItemCollection"),
+		panelTemplate = require("text!./template/panelTemplate"),
+		panelButtonTemplate = require("text!./template/panelButtonTemplate"),
 		epoxyViewMixin = require("common/mixin/epoxyViewMixin");
 
 	var Panel = Backbone.View.extend({
 
 		el: function() {
-			return _.template(this.template)({
-				aditionalCssClass: this.aditionalCssClass
+			return _.template(panelTemplate)({
+				aditionalCssClass: this.aditionalCssClass,
+				title: this.title
 			});
 		},
 
@@ -17,12 +21,33 @@ define(function(require) {
 
 			this.contentTemplate = options.contentTemplate;
 			this.model = options.model;
+			this.title = options.title || "";
+			this.buttons = options.buttons && (new ItemCollection({
+				eventPrefix: "button",
+				items: options.buttons,
+				itemTemplate: panelButtonTemplate
+			}));
 
 			this.epoxifyView();
+
+			this.initEvents();
+		},
+
+		initEvents: function() {
+			this.listenTo(this.buttons, "button:selected", _.bind(this._onButtonSelected, this));
+		},
+
+		_onButtonSelected: function(buttonView, buttonModel) {
+			// TODO
+		},
+
+		_renderButtons: function() {
+			this.buttons && this.$el.find(".footer").append(this.buttons.render().$el);
 		},
 
 		render: function() {
-			this.$el.append(_.template(this.contentTemplate)({model: this.model}));
+			this.$el.find(".content").append(_.template(this.contentTemplate)({model: this.model.toJSON()}));
+			this._renderButtons();
 			this.applyEpoxyBindings();
 			return this;
 		},
